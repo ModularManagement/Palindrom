2ba6c37a49f5d7c1ed1c6cbe889d500d1a3209de
eriksunsol <erik.sundkvist@modularmanagement.com>
2020-09-23 08:16:19 +0200

    Set up webpack with jsonpatcherproxy v0.1.0-0 without ES modules.

--- src/palindrom.js
+++ src/palindrom.js
@@ -5,7 +5,7 @@
  */
 
 /* this variable is bumped automatically when you call npm version */
-const palindromVersion = '6.0.0';
+const palindromVersion = '6.0.0-JSONPatcherProxy-0.1.0-0-no-es-modules';
 
 const CLIENT = 'Client';
 const SERVER = 'Server';
